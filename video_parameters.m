3788edb96655828954e0ad0027728dd8fa31e2bc
Melissa Shaw <ms1121@leicester.ac.uk>
2020-05-13 14:35:48 +0100

    added parameter file for full videos

---
+++ video_parameters.m
@@ -0,0 +1,79 @@
+% Structs for each video
+i=0;
+
+% Video 1
+i=i+1;
+v(i).animal="A1";
+v(i).basefilepath='Deep_Lab_Cut/Orig_Videos/MD200312_A_BD/A_WIN_20200326/A_WIN_20200326_18_15_04_ProDLC_resnet50_Full_TrackingApr17shuffle1_2400filtered.csv'; % path to original video data
+v(i).trackpath=v(i).basefilepath;
+%v(i).trackpath='Tracking Results/A1_edited.csv'; % path to edited coordinate track
+v(i).track=readmatrix(v(i).trackpath);
+v(i).actual_length=0.085; % Approx real mouse length (m) from nose to tailbase
+v(i).coord_length=159; % Approx mouse coordinate length from nose to tailbase
+v(i).conv_factor=v(i).actual_length/v(i).coord_length; % approx coordinate distance in metres
+v(i).imagepath='Deep_Lab_Cut/Still_Images/A1.png'; % path to still image
+v(i).num_frames=27001; % start position + 15 mins at 30FPS
+v(i).FPS=30;
+v(i).cage_bounds=[78,150,53,75]; % [low_x,upp_x,low_y,upp_y], upp_x/y is to be taken away from x/y image size
+v(i).movement=[];
+v(i).dir_speeds=[];
+v(i).nest_bounds=[766,0,309,0]; % [low_x,upp_x,low_y,upp_y], upp_x/y is to be taken away from x/y image size
+v(i).nest_times=[];
+
+% Video 2
+i=i+1;
+v(i).animal="A2";
+v(i).basefilepath='Deep_Lab_Cut/Orig_Videos/MD200312_A_BD/A_WIN_20200327/A_WIN_20200327_18_20_59_ProDLC_resnet50_Full_TrackingApr17shuffle1_2400filtered.csv'; % path to original video data
+v(i).trackpath=v(i).basefilepath;
+%v(i).trackpath='Tracking Results/A2_edited.csv'; % path to edited coordinate track
+v(i).track=readmatrix(v(i).trackpath);
+v(i).actual_length=0.085; % Approx real mouse length (m) from nose to tailbase
+v(i).coord_length=169; % Approx mouse coordinate length from nose to tailbase
+v(i).conv_factor=v(i).actual_length/v(i).coord_length; % approx coordinate distance in metres
+v(i).imagepath='Deep_Lab_Cut/Still_Images/A2.png'; % path to still image
+v(i).num_frames=27001; % start position + 15 mins at 30FPS
+v(i).FPS=30;
+v(i).cage_bounds=[0,0,0,0]; % [low_x,upp_x,low_y,upp_y], upp_x/y is to be taken away from x/y image size
+v(i).movement=[];
+v(i).dir_speeds=[];
+v(i).nest_bounds=[0,742,0,308]; % [low_x,upp_x,low_y,upp_y], upp_x/y is to be taken away from x/y image size
+v(i).nest_times=[];
+
+% Video 3
+i=i+1;
+v(i).animal="B1";
+v(i).basefilepath='Deep_Lab_Cut/Orig_Videos/MD200312_B_BD/B_WIN_20200327/B_WIN_20200327_13_40_18_ProDLC_resnet50_Full_TrackingApr17shuffle1_2400filtered.csv'; % path to original video data
+v(i).trackpath=v(i).basefilepath;
+%v(i).trackpath='Tracking Results/B1_edited.csv'; % path to edited coordinate track
+v(i).track=readmatrix(v(i).trackpath);
+v(i).actual_length=0.085; % Approx real mouse length (m) from nose to tailbase
+v(i).coord_length=211; % Approx mouse coordinate length from nose to tailbase
+v(i).conv_factor=v(i).actual_length/v(i).coord_length; % approx coordinate distance in metres
+v(i).imagepath='Deep_Lab_Cut/Still_Images/B1.png'; % path to still image
+v(i).num_frames=27001; % start position + 15 mins at 30FPS
+v(i).FPS=30;
+v(i).cage_bounds=[0,18,0,16]; % [low_x,upp_x,low_y,upp_y], upp_x/y is to be taken away from x/y image size
+v(i).movement=[];
+v(i).dir_speeds=[];
+v(i).nest_bounds=[769,0,303,0]; % [low_x,upp_x,low_y,upp_y], upp_x/y is to be taken away from x/y image size
+v(i).nest_times=[];
+
+% Video 4
+i=i+1;
+v(i).animal="B2";
+v(i).basefilepath='Deep_Lab_Cut/Orig_Videos/MD200312_B_BD/B_WIN_20200328/B_WIN_20200328_19_27_06_ProDLC_resnet50_Full_TrackingApr17shuffle1_2400filtered.csv'; % path to original video data
+v(i).trackpath=v(i).basefilepath;
+%v(i).trackpath='Tracking Results/B2_edited.csv'; % path to edited coordinate track
+v(i).track=readmatrix(v(i).trackpath);
+v(i).actual_length=0.085; % Approx real mouse length (m) from nose to tailbase
+v(i).coord_length=251; % Approx mouse coordinate length from nose to tailbase
+v(i).conv_factor=v(i).actual_length/v(i).coord_length; % approx coordinate distance in metres
+v(i).imagepath='Deep_Lab_Cut/Still_Images/B2.png'; % path to still image
+v(i).num_frames=27001; % start position + 15 mins at 30FPS
+v(i).FPS=30;
+v(i).cage_bounds=[0,0,0,88]; % [low_x,upp_x,low_y,upp_y], upp_x/y is to be taken away from x/y image size
+v(i).movement=[];
+v(i).dir_speeds=[];
+v(i).nest_bounds=[757,0,220,0]; % [low_x,upp_x,low_y,upp_y], upp_x/y is to be taken away from x/y image size
+v(i).nest_times=[];
+
